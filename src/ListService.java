f92d0da9593a99c012b5623d113dd62c2ccfafd7
Jeanne Kazakova <johndoe@example.comjeannekazakova@gmail.com>
2021-12-11 10:00:18 +0300

    1

--- src/ListService.java
+++ src/ListService.java
@@ -1,2 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * [11:12] Пунченко Евгений
+ * Создать набор классов для работы со списком, два метода
+ * - первый принимает N и возвращает список N первых чисел,
+ * второй метод принимает N возвращает первые N четных чисел
+ * .Вторая ветка создать класс Автомобиль и реализовать наследование
+ * от транспортного средства
+ */
 public class ListService {
+
+    public static List<Integer> numEven(int n) {
+        return IntStream.rangeClosed(0, n).boxed().collect(Collectors.toList());
+    }
 }
